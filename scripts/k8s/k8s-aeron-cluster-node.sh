0d4ba12fb3d7ff340cc94718742685c3f151840f
Bernardo Salazar <bernardo.salazar@weareadaptive.com>
2025-09-29 14:37:04 +0200

    feat(k8s): add thread pinning on aeron cluster on k8s

--- scripts/k8s/k8s-aeron-cluster-node.sh
+++ scripts/k8s/k8s-aeron-cluster-node.sh
@@ -11,16 +11,22 @@ source "${DIR}/k8s-common"
 f_benchmark_pre
 
 # Starts the cluster node with a restricted CPU affinity
-echo "** Starting with base cpu core ${CGROUP_CPUSETS[2]}"
-taskset -c "${CGROUP_CPUSETS[2]}" "${DIR}/../aeron/cluster-node" "${@}" &
+echo "** Starting with base cpu core ${CGROUP_CPUSETS[0]}"
+taskset -c "${CGROUP_CPUSETS[0]}" "${DIR}/../aeron/cluster-node" "${@}" &
 
 # Wait for Java process to be up
 f_wait_for_process 'io.aeron.benchmarks.aeron.ClusterNode'
 
-f_wait_for_thread 'clustered-servi' 3
+# Waits for thread to start (in seconds) and sets its affinity
+f_wait_for_thread 'clustered-servi' 3 #TODO: consider f_wait_for_threads and pass an array of thread names
 
-# Sets the affinities main cluster node thread
-f_pin_thread "clustered-servi" "${CGROUP_CPUSETS[3]}"
+# Sets the affinities for high performance threads
+f_pin_thread "clustered-servi" "${CGROUP_CPUSETS[0]}"
+f_pin_thread "archive-recorde" "${CGROUP_CPUSETS[1]}"
+f_pin_thread "archive-replaye" "${CGROUP_CPUSETS[2]}"
+f_pin_thread "archive-conduct" "${CGROUP_CPUSETS[3]}"
+f_pin_thread "consensus-modul" "${CGROUP_CPUSETS[4]}"
+f_pin_thread "aeron-client-co" "${CGROUP_CPUSETS[5]}"
 
 # Wait for all background tasks
 fg
