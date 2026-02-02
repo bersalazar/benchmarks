b77debb513d74d2bf5070508f77fde0df342da7e
Bernardo Salazar <bernardo.salazar@weareadaptive.com>
2026-02-02 22:59:42 +0100

    fix: remove egress from cluster opts

--- scripts/k8s-remote-testing.sh
+++ scripts/k8s-remote-testing.sh
@@ -99,93 +99,9 @@ if [[ "${TEST_TO_RUN}" == *cluster* ]]; then
   kubectl -n "${K8S_NAMESPACE}" wait --timeout=90s --for=condition=ContainersReady=true pod/aeron-cluster-1
   kubectl -n "${K8S_NAMESPACE}" wait --timeout=90s --for=condition=ContainersReady=true pod/aeron-cluster-2
 
-  # Get the media driver IPs from the cluster pods
-  if [[ "${TEST_TO_RUN}" =~ .*-dpdk$ ]]; then
-    AERON_0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec -it aeron-cluster-0 -c aeronmd-dpdk -- bash -c 'echo ${PCIDEVICE_INTEL_COM_AWS_DPDK_INFO}' | jq -r '.. | ."IPV4_ADDRESS"? | select(. != null)')"
-    AERON_1_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec -it aeron-cluster-1 -c aeronmd-dpdk -- bash -c 'echo ${PCIDEVICE_INTEL_COM_AWS_DPDK_INFO}' | jq -r '.. | ."IPV4_ADDRESS"? | select(. != null)')"
-    AERON_2_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec -it aeron-cluster-2 -c aeronmd-dpdk -- bash -c 'echo ${PCIDEVICE_INTEL_COM_AWS_DPDK_INFO}' | jq -r '.. | ."IPV4_ADDRESS"? | select(. != null)')"
-  elif [[ "${TEST_TO_RUN}" =~ .*-java$ ]]; then
-    AERON_0_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-0  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-    AERON_1_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-1  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-    AERON_2_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-2  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  elif [[ "${TEST_TO_RUN}" =~ .*-c$ ]]; then
-    AERON_0_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-0  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-    AERON_1_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-1  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-    AERON_2_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-2  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  else
-    f_log "Media driver config not found for ${TEST_TO_RUN}"
-    exit 1
-  fi
-
-  f_log "Found media driver IPs:"
-  echo "aeron-cluster-0: ${AERON_0_MD_IP}"
-  echo "aeron-cluster-1: ${AERON_1_MD_IP}"
-  echo "aeron-cluster-2: ${AERON_2_MD_IP}"
-
-  # Generate endpoint slice with IPs.
-  # Because we use interfaces that have no obvious IPs, we need to have a way to generate DNS records for the test.
-#   f_log "Generating EndpointSlice with DNS for media driver IPs"
-#   ENDPOINT_SLICE=$(cat <<EOF
-# ---
-# apiVersion: discovery.k8s.io/v1
-# kind: EndpointSlice
-# metadata:
-#   name: aeron-cluster-md
-#   labels:
-#     kubernetes.io/service-name: aeron-cluster
-#     endpointslice.kubernetes.io/managed-by: caf-benchmark
-# addressType: IPv4
-# endpoints:
-#   - addresses:
-#       - ${AERON_0_MD_IP}
-#     hostname: aeron-cluster-0
-#   - addresses:
-#       - ${AERON_1_MD_IP}
-#     hostname: aeron-cluster-1
-#   - addresses:
-#       - ${AERON_2_MD_IP}
-#     hostname: aeron-cluster-2
-# EOF
-# )
-#   # Create cluster endpoint slice
-#   echo "${ENDPOINT_SLICE}" | kubectl -n "${K8S_NAMESPACE}" apply -f -
-
   f_log "Deploying aeron cluster client: ${CLIENT_DEPLOY}"
   kubectl --namespace "${K8S_NAMESPACE}" apply --wait=true --kustomize "k8s/${CLIENT_DEPLOY}"
   kubectl -n "${K8S_NAMESPACE}" wait --timeout=90s --for=condition=ContainersReady=true pod/aeron-cluster-client
-
-  # Get the media driver IPs from the client pod
-  if [[ "${TEST_TO_RUN}" =~ .*-dpdk$ ]]; then
-    AERON_CLIENT_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec -it aeron-cluster-client -c aeronmd-dpdk -- bash -c 'echo ${PCIDEVICE_INTEL_COM_AWS_DPDK_INFO}' | jq -r '.. | ."IPV4_ADDRESS"? | select(. != null)')"
-  elif [[ "${TEST_TO_RUN}" =~ .*-java$ ]]; then
-    AERON_CLIENT_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-client  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  elif [[ "${TEST_TO_RUN}" =~ .*-c$ ]]; then
-    AERON_CLIENT_MD_IP="$(kubectl --namespace "${K8S_NAMESPACE}" exec aeron-cluster-client  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} | jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  else
-    f_log "Media driver config not found for ${TEST_TO_RUN}"
-    exit 1
-  fi
-
-  f_log "Found media driver IP for aeron-cluster-client: ${AERON_CLIENT_MD_IP}"
-
-  f_log "Generating EndpointSlice with DNS for aeron cluster client media driver IP"
-  ENDPOINT_SLICE=$(cat <<EOF
----
-apiVersion: discovery.k8s.io/v1
-kind: EndpointSlice
-metadata:
-  name: aeron-cluster-client-md
-  labels:
-    kubernetes.io/service-name: aeron-cluster-client
-    endpointslice.kubernetes.io/managed-by: caf-benchmark
-addressType: IPv4
-endpoints:
-  - addresses:
-      - ${AERON_CLIENT_MD_IP}
-    hostname: aeron-cluster-client
-EOF
-)
-  echo "${ENDPOINT_SLICE}" | kubectl -n "${K8S_NAMESPACE}" apply -f -
 
   # When the benchmark finishes, the benchmark containers stop, generating a NotReady condition
   f_log "Waiting for benchmarks to finish"
@@ -218,30 +134,9 @@ EOF
 
 else
   f_log "Generating new benchmark setup for: ${TEST_TO_RUN}"
-
   kubectl --namespace "${K8S_NAMESPACE}" apply --wait=true --kustomize "k8s/${TEST_TO_RUN}"
   kubectl -n "${K8S_NAMESPACE}" wait --timeout=90s --for=condition=ContainersReady=true pod/aeron-benchmark-0
   kubectl -n "${K8S_NAMESPACE}" wait --timeout=90s --for=condition=ContainersReady=true pod/aeron-benchmark-1
-
-  # # DPDK Media Driver
-  # if [[ "${TEST_TO_RUN}" =~ .*-dpdk$ ]]; then
-  #   AERON_0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec -it aeron-benchmark-0 -c aeronmd-dpdk -- bash -c 'echo ${PCIDEVICE_INTEL_COM_AWS_DPDK_INFO}' | jq -r '.. | ."IPV4_ADDRESS"? | select(. != null)')"
-  #   AERON_CLIENT_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec -it aeron-benchmark-1 -c aeronmd-dpdk -- bash -c 'echo ${PCIDEVICE_INTEL_COM_AWS_DPDK_INFO}' | jq -r '.. | ."IPV4_ADDRESS"? | select(. != null)')"
-  # # Java Media Driver
-  # elif [[ "${TEST_TO_RUN}" =~ .*-java$ ]]; then
-  #   AERON_0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-0  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  #   AERON_CLIENT_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-1  -c aeronmd-java -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  # elif [[ "${TEST_TO_RUN}" =~ .*-c$ ]]; then
-  #   AERON_0_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-0  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  #   AERON_CLIENT_MD_IP="$(kubectl -n "${K8S_NAMESPACE}" exec aeron-benchmark-1  -c aeronmd-c -- bash -c "ip -4 -json addr show ${INTERFACE} |   jq -r '.[] | .addr_info[] | select(.family == \"inet\") | .local'")"
-  # else
-  #   f_log "Media driver config not found"
-  #   exit 1
-  # fi
-
-  f_log "Found Media driver IPs:"
-  echo "aeron-benchmark-0: ${AERON_0_MD_IP}"
-  echo "aeron-benchmark-1: ${AERON_CLIENT_MD_IP}"
 
   # When the benchmark finishes, the benchmark containers stop, generating a NotReady condition
   f_log "Waiting for benchmarks to finish"
